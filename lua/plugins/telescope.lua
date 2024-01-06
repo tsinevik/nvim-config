c13f6774ce3604fbae42da72c28e4f5055c9688b
tsinevik <braid98@yandex.ru>
2024-01-06 20:44:12 +0400

    Display dotfiles for find_files in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.4",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			pickers = {
+				find_files = {
+					find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+				},
+			},
+		})
+	end,
 }
